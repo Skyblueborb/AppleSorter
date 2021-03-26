7c42c1bacc0fca1fa588fb5160764cb1d3c992e6
Skyblueborb <tomaszborbely0710@gmail.com>
2021-03-26 14:44:00 +0100

    makedirs func

--- sortowanie.py
+++ sortowanie.py
@@ -39,39 +39,43 @@ class Sorting:
         pass
 
     def sort(self, x, y):
-        try: # Tries to make dirs only applicable if first time using the program
-            os.makedirs(documentsLocation)
-            os.makedirs(mediaLocation)
-            os.makedirs(setupFilesLocation)
-            os.makedirs(compressedFilesLocation)
-            os.makedirs(elseLocation)
-        except OSError:
-            pass
-            for file in dir_path: # Finds all files in the specified directory
-                extension = os.path.splitext(file)[1]
-                if os.path.isfile(file) and extension in y: # Filters files and filenames
+        self.makeDirs()
+        for file in dir_path: # Finds all files in the specified directory
+            extension = os.path.splitext(file)[1]
+            if os.path.isfile(file) and extension in y: # Filters files and filenames
+                try:
+                    shutil.move(file, x) # Tries to move them
+                except OSError:
+                    # Gets nessecary info about the file
+                    onlyFilename = ntpath.basename(file)
+                    filenamePlusOne = f'{downloadFolder}{onlyFilename} (1){extension}'
                     try:
-                        shutil.move(file, x) # Tries to move them
-                    except OSError:
-                        # Gets nessecary info about the file
-                        onlyFilename = ntpath.basename(file)
-                        filenamePlusOne = f'{downloadFolder}{onlyFilename} (1){extension}'
-                        try:
-                            # Finds the index of the number to be changed
-                            indexOfCharacter = onlyFilename.index(f'(') + 1
-                            # Variables of the numbers
-                            number = onlyFilename[indexOfCharacter]
-                            numberIntPlusOne = str(int(number) + 1)
-                            # Figures out how how to rename it
-                            filenameRenamed = onlyFilename[:indexOfCharacter] + numberIntPlusOne + onlyFilename[indexOfCharacter+1:]
-                            wholePathFileRenamed = f"{downloadFolder}{filenameRenamed}{extension}"
-                            os.rename(file, wholePathFileRenamed) # Renames it
-                            shutil.move(wholePathFileRenamed, x) # Finally it move it
-                        except ValueError:
-                            os.rename(file, filenamePlusOne)
-                            shutil.move(filenamePlusOne, x)
-                            pass
+                        # Finds the index of the number to be changed
+                        indexOfCharacter = onlyFilename.index(f'(') + 1
+                        # Variables of the numbers
+                        number = onlyFilename[indexOfCharacter]
+                        numberIntPlusOne = str(int(number) + 1)
+                        # Figures out how how to rename it
+                        filenameRenamed = onlyFilename[:indexOfCharacter] + numberIntPlusOne + onlyFilename[indexOfCharacter+1:]
+                        wholePathFileRenamed = f"{downloadFolder}{filenameRenamed}{extension}"
+                        os.rename(file, wholePathFileRenamed) # Renames it
+                        shutil.move(wholePathFileRenamed, x) # Finally it move it
+                    except ValueError:
+                        os.rename(file, filenamePlusOne)
+                        shutil.move(filenamePlusOne, x)
                         pass
+                    pass
+
+    def makeDirs(self): # Tries to make dirs only applicable if first time using the program
+        allDirs = [documentsLocation, mediaLocation, setupFilesLocation, compressedFilesLocation, elseLocation]
+        timeRun = 0
+        while timeRun <= 4:
+            try:
+                os.makedirs(allDirs[timeRun])
+                timeRun += 1
+            except FileExistsError:
+                timeRun += 1
+                pass
 
     def elsesort(self): # Sorts files depending on excluded variable
         for file in dir_path:
